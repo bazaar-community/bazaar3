8d2299005f492a37bbe2a87150cf896e3fcc71d0
Suleyman Poyraz <zaryob.dev@gmail.com>
2020-12-18 12:22:24 +0300

    Revert "Added debian patch "30_test_too_much""
    This reverts commit 3f065820c12652baf63fedd67ec74cff26af79ba.

--- bzrlib/tests/blackbox/__init__.py
+++ bzrlib/tests/blackbox/__init__.py
@@ -124,6 +124,7 @@ def load_tests(basic_tests, module, loader):
                      'test_switch',
                      'test_tags',
                      'test_testament',
+                     'test_too_much',
                      'test_uncommit',
                      'test_unknowns',
                      'test_update',
